66a1b7cdc7efe8f0246bfd019373966d42f819c0
MasonQ123 <mason117167@student-schoolsofwestfield.org>
2025-09-17 09:05:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
 
 /* Var Alert */
 // make a string variable for a color, set it equal to your favorite
+var color = "blue";
 // alert the message" "My favorite color is " plus the color
-
+alert("My favorite color is "+ color);
 /* Var Alert Prompt */
 // make a string variable for color, prompt the user for their favorite
 // alert color " is a nice color!"
@@ -15,3 +16,13 @@
 // make a variable for color, prompt the user for their favorite
 // if user enters "black" alert "Black is not really a color." 
 // otherwise alert color " is a nice color!"
+
+/* AGE KATAS */
+
+/* Var Alert */
+// make an integer variable for age, set it equal to your age
+var age = 17;
+// make a string variable for name, set it to your name
+var fname = "Mason";
+// alert name is age years old
+alert(fname + " is " + age + " years old.");
