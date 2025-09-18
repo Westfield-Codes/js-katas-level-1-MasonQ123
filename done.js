424abf3d6f35f617542101fceb1de93db55684c6
MasonQ123 <mason117167@student-schoolsofwestfield.org>
2025-09-18 09:24:36 -0400

    update

--- done.js
+++ done.js
@@ -2,6 +2,13 @@
 
 /* COLOR KATAS */
 
+/* Var Alert */
+// make a string variable for a color, set it equal to your favorite
+var color = "blue";
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is "+ color);
+
+
 /* No Prompt */
 
 
@@ -12,6 +19,13 @@
 
 /* AGE */
 
+/* Var Alert */
+// make an integer variable for age, set it equal to your age
+var age = 17;
+// make a string variable for name, set it to your name
+var fname = "Mason";
+// alert name is age years old
+alert(fname + " is " + age + " years old.");
 /* No Prompt */
 
 
@@ -22,7 +36,13 @@
 
 
 /* ICE CREAM */
-
+/* Var Alert Prompt */
+// make a string variable for favorite ice cream flavor, prompt user for it
+var flavor = prompt("Favorite flavor?");
+// make an integer variable for number of scoops on the cone, prompt user for it
+var scoops = prompt("How many scoops?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want " + scoops + " scoops of " + flavor + " flavored ice cream.");
 /* No Prompt */
 
 
@@ -33,7 +53,13 @@
 
 
 /* PET */
-
+/* Var Alert Prompt */
+// make a variable for a pet type, prompt user for it
+var pet = prompt("What type of pet do you have?");
+// make a variable for a pet name, prompt user for it
+var pname = prompt("What's their name?")
+// alert "You have a pet type named pet name 
+alert("You have a " + pet + " named " + pname);
 /* No Prompt */
 
 
